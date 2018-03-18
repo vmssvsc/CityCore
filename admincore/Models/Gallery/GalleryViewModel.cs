47248448c869690574816902b2383ee3fadf6853
Ankit Patel <AGNI@DESKTOP-N4MLJF4>
2018-03-18 12:10:57 +0530

    admin: gallery

---
+++ admincore/Models/Gallery/GalleryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace admincore.Models.Gallery
+{
+    public class GalleryViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+       
+        public List <IFormFile> Files{ get; set; }
+    }
+}
