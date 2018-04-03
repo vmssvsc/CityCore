4a581c3fd45323cf53c1f3d9349004332caf41c6
vmssvsc <vmssvsc@gmail.com>
2018-04-03 23:12:41 +0530

    Database - Added title column to Project Initiative.

---
+++ admincore/Data/Migrations/20180403174108_AddedColumnToInitiativeTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace admincore.Data.Migrations
+{
+    public partial class AddedColumnToInitiativeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "ProjectInitiatives",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "ProjectInitiatives");
+        }
+    }
+}
