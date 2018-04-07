b50ff241365aacfdc7c856e4bceed648a24b877f
PriyankParmar-987 <priyankparmar175@gmail.com>
2018-04-08 00:48:43 +0530

    Admin- ABD project

---
+++ admincore/Models/Project/ProjectInititativeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace admincore.Models.Project
+{
+    public class ProjectInititativeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+
+        [Required]
+        [MaxLength(200)]
+        public string Initiative { get; set; }
+
+
+    }
+}
---
+++ admincore/wwwroot/js/Project/ABDInitiative.js
@@ -0,0 +1,112 @@
+﻿InitiativeList = {
+    Variables: {
+        srcEdit: '/Project/SaveI',
+        srcList: '/Project/GetListI',
+        srcDelete: '/Project/DeleteI',
+        oTable: null,
+    },
+    Controls: {
+        table: '#tblABD',
+    },
+    IntializeTable: function () {
+        InitiativeList.Variables.oTable = $(InitiativeList.Controls.table).dataTable({
+            "sAjaxSource": InitiativeList.Variables.srcList,
+            //"aaSorting": [[1, "desc"]],// default sorting
+            "sDom": "frtlip",
+            "autoWidth": false,
+            "bLengthChange": true,
+            "fixedHeader": true,
+            "aoColumnDefs": [
+                {
+                    "aTargets": [0],
+                    "visible": false
+                },
+                {
+                    "aTargets": [1],
+                },
+                {
+                    "aTargets": [2],
+                },
+
+                {
+                    "aTargets": [3],
+                    "mRender": function (data, type, full) {
+                        return "<a href='/Project/AddEditI?id=" + full[0] + "' ><i class='fa fa-edit'></i></a><a href='javascript:void(0)' onClick=\"\VideoList.DeleteInitiative('" + full[0] + "')\"\><i class='fa fa-trash-o'></i></a>";
+                    },
+                    "sortable": false,
+                    "className": "text-center",
+                }
+
+            ],
+
+            "oLanguage": {
+                "sEmptyTable": $('#hdnNodataavailable').val(),
+                "sLengthMenu": "Page Size: _MENU_",
+                "oPaginate": {
+                    "sFirst": $('#hdnFirst').val(),
+                    "sLast": $('#hdnLast').val(),
+                    "sNext": $('#hdnNext').val(),
+                    "sPrevious": $('#hdnPrevious').val()
+                }
+            },
+            "fnServerParams": function (aoData) {
+                $("div").data("srchParams",
+                    [
+                        //{ name: 'iDisplayLength', value: $("#hdnGeneralPageSize").val() },
+                        { name: 'srchTxt', value: encodeURIComponent($(InitiativeList.Controls.txtSearch).val() == '' ? '' : $(InitiativeList.Controls.txtSearch).val()) },
+                        { name: 'srchBy', value: 'ALL' },
+
+
+                        //{
+                        //    name: 'date', value: $('#txtDate').val().toString().trim() == "" ? "" : common.formatDate($('#txtDate').val().toString().trim()), //($('#ddlStationId option:selected').val() == "Select" || $('#ddlStationId option:selected').val() == "0") ? "" : $('#ddlStationId option:selected').val()
+                        //},
+                    ]);
+
+                var srchParams = $("div").data("srchParams");
+                if (srchParams) {
+                    for (var i = 0; i < srchParams.length; i++) {
+                        aoData.push({ "name": "" + srchParams[i].name + "", "value": "" + srchParams[i].value + "" });
+                    }
+                }
+            },
+            "fnInfoCallback": function (oSettings, iStart, iEnd, iMax, iTotal, sPre) {
+                // return common.pagingText(iStart, iEnd, iTotal, $('#hdnRecordsText').val(), oSettings._iDisplayLength);
+
+            },
+        });
+    },
+
+    reloadList: function () {
+        $(InitiativeList.Controls.table).DataTable().ajax.reload();
+    },
+
+    DeleteInitiative: function (id) {
+        bootbox.confirm("Are you sure you want to delete this  ABD Area Initiative ?",
+            function (result) {
+                if (result) {
+                    $.ajax({
+                        type: 'get',
+                        url: InitiativeList.Variables.srcDelete + '?id=' + id,
+                        success: function (data) {
+                            if (data.success) {
+                                Common.Success(data.message);
+                                InitiativeList.reloadList();
+                            }
+                            else {
+                                Common.Error(data.message);
+                            }
+                        },
+                        error: function () {
+                            Common.Error(data.message);
+                        }
+                    });
+                }
+            });
+
+    }
+
+};
+
+$(document).ready(function () {
+    InitiativeList.IntializeTable();
+});
