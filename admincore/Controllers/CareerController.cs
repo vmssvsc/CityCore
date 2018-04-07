adea762e9f1cbeb4fc906f08af53e1c72deafa5b
Ankit Patel <AGNI@DESKTOP-N4MLJF4>
2018-04-07 19:51:57 +0530

    admin core & citycore : career pages MVC

---
+++ admincore/Controllers/CareerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace admincore.Controllers
+{
+    public class CareerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddEdit()
+        {
+            return View();
+        }
+    }
+}
---
+++ admincore/wwwroot/js/Carrer/Index.js
@@ -0,0 +1,121 @@
+﻿CareerList = {
+    Variables: {
+        srcEdit: '/Career/Save',
+        srcList: '/Career/GetList',
+        srcDelete: '/Career/Delete',
+        oTable: null,
+    },
+    Controls: {
+        table: '#tblCareer',
+    },
+    IntializeTable: function () {
+        CareerList.Variables.oTable = $(CareerList.Controls.table).dataTable({
+            "sAjaxSource": CareerList.Variables.srcList,
+            //"aaSorting": [[1, "desc"]],// default sorting
+            "sDom": "frtlip",
+            "autoWidth": false,
+            "bLengthChange": true,
+            "fixedHeader": true,
+            "aoColumnDefs": [
+                {
+                    "aTargets": [0],
+                    "visible": false
+                },
+                {
+                    "aTargets": [1],
+                },
+                {
+                    "aTargets": [2],
+                },
+                {
+                    "aTargets": [3],
+                },
+                {
+                    "aTargets": [4],
+                },
+                {
+                    "aTargets": [5],
+                },
+
+                {
+                    "aTargets": [6],
+                    "mRender": function (data, type, full) {
+                        return "<a href='/Career/AddEdit?id=" + full[0] + "' ><i class='fa fa-edit'></i></a><a href='javascript:void(0)' onClick=\"\VideoList.DeleteCareer('" + full[0] + "')\"\><i class='fa fa-trash-o'></i></a>";
+                    },
+                    "sortable": false,
+                    "className": "text-center",
+                }
+
+            ],
+
+            "oLanguage": {
+                "sEmptyTable": $('#hdnNodataavailable').val(),
+                "sLengthMenu": "Page Size: _MENU_",
+                "oPaginate": {
+                    "sFirst": $('#hdnFirst').val(),
+                    "sLast": $('#hdnLast').val(),
+                    "sNext": $('#hdnNext').val(),
+                    "sPrevious": $('#hdnPrevious').val()
+                }
+            },
+            "fnServerParams": function (aoData) {
+                $("div").data("srchParams",
+                    [
+                        //{ name: 'iDisplayLength', value: $("#hdnGeneralPageSize").val() },
+                        { name: 'srchTxt', value: encodeURIComponent($(CarrerList.Controls.txtSearch).val() == '' ? '' : $(CarrerList.Controls.txtSearch).val()) },
+                        { name: 'srchBy', value: 'ALL' },
+
+
+                        //{
+                        //    name: 'date', value: $('#txtDate').val().toString().trim() == "" ? "" : common.formatDate($('#txtDate').val().toString().trim()), //($('#ddlStationId option:selected').val() == "Select" || $('#ddlStationId option:selected').val() == "0") ? "" : $('#ddlStationId option:selected').val()
+                        //},
+                    ]);
+
+                var srchParams = $("div").data("srchParams");
+                if (srchParams) {
+                    for (var i = 0; i < srchParams.length; i++) {
+                        aoData.push({ "name": "" + srchParams[i].name + "", "value": "" + srchParams[i].value + "" });
+                    }
+                }
+            },
+            "fnInfoCallback": function (oSettings, iStart, iEnd, iMax, iTotal, sPre) {
+                // return common.pagingText(iStart, iEnd, iTotal, $('#hdnRecordsText').val(), oSettings._iDisplayLength);
+
+            },
+        });
+    },
+
+    reloadList: function () {
+        $(CareerList.Controls.table).DataTable().ajax.reload();
+    },
+
+    DeleteCareer: function (id) {
+        bootbox.confirm("Are you sure you want to delete this Video?",
+            function (result) {
+                if (result) {
+                    $.ajax({
+                        type: 'get',
+                        url: CareerList.Variables.srcDelete + '?id=' + id,
+                        success: function (data) {
+                            if (data.success) {
+                                Common.Success(data.message);
+                                CareerList.reloadList();
+                            }
+                            else {
+                                Common.Error(data.message);
+                            }
+                        },
+                        error: function () {
+                            Common.Error(data.message);
+                        }
+                    });
+                }
+            });
+
+    }
+
+};
+
+$(document).ready(function () {
+    CareerList.IntializeTable();
+});
