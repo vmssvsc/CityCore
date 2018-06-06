4618ec5c80f9c4f504d436e68fd92751f601e51f
Ankit Patel <AGNI@DESKTOP-N4MLJF4>
2018-06-06 22:24:49 +0530

    #tender desigin both admin and user

---
+++ CityCore/Controllers/TenderController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityCore.Controllers
+{
+    public class TenderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
